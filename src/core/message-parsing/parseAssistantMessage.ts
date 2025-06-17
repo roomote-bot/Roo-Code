946cc0cae2e2306cb92f3ff49f0da39753046673
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2025-06-17 10:37:50 +0700

    remove old parseAssistantMessage stuff

--- src/core/message-parsing/parseAssistantMessage.ts
+++
@@ -1,9 +0,0 @@
-import { DirectiveStreamingParser } from "./DirectiveStreamingParser"
-import type { Directive } from "./directives"
-
-// Backward compatibility alias
-export type AssistantMessageContent = Directive
-
-export function parseAssistantMessage(assistantMessage: string): Directive[] {
-	return DirectiveStreamingParser.parse(assistantMessage)
-}
